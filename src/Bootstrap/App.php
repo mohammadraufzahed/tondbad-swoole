ee3fa978060917e21e1170ec19e56ec2136b87ed
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2024-09-19 12:54:12 +0330

    Add App class to bootstrap application with provider registration and server initialization
    - Introduce `App` class in `TondbadSwoole\Bootstrap` namespace.
    - Implement provider registration and bootstrapping.
    - Add logic to start either HTTP or gRPC server based on configuration.

---
+++ src/Bootstrap/App.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TondbadSwoole\Bootstrap;
+
+use OpenSwoole\GRPC\Server as GrpcServer;
+use OpenSwoole\WebSocket\Server as HttpServer;
+use TondbadSwoole\Core\Config;
+use TondbadSwoole\Core\Container;
+use TondbadSwoole\Providers\Contracts\ServiceProviderInterface;
+
+class App
+{
+    private readonly Container $container;
+    /**
+     * @var ServiceProviderInterface[]
+     */
+    private readonly array $providers;
+
+    public function __construct()
+    {
+        $this->container = Container::create();
+        $this->providers = Config::get('providers', []);
+        $this->registerProviders();
+        $this->bootProviders();
+    }
+
+    protected function registerProviders(): void
+    {
+        foreach ($this->providers as $providerClass) {
+            $provider = new $providerClass();
+            $provider->beforeRegister($this->container);
+            $provider->register($this->container);
+            $provider->afterRegister($this->container);
+        }
+    }
+
+    protected function bootProviders(): void
+    {
+        foreach ($this->providers as $providerClass) {
+            $provider = new $providerClass();
+            $provider->beforeBoot($this->container);
+            $provider->boot($this->container);
+            $provider->afterBoot($this->container);
+        }
+    }
+
+    public function run(): void
+    {
+        if (Config::get('app.type', 'http') === 'http') {
+            $httpServer = $this->container->make(HttpServer::class);
+            $httpServer->start();
+        } else {
+            $grpcServer = $this->container->make(GrpcServer::class);
+            $grpcServer->start();
+        }
+    }
+}
