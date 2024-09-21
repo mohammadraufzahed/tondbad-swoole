a03e557aa16efbe71c3786c6ae85f3162a23c9da
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2024-09-21 12:16:44 +0330

    Add detailed docblocks and new methods to ServiceProvider contract
    - Added docblocks to existing methods for better documentation and clarity.
    - Introduced `getPriority` method to specify service provider priority.
    - Added `addConfigurationPath` method to manage configuration file paths.

--- src/Providers/Contracts/ServiceProvider.php
+++ src/Providers/Contracts/ServiceProvider.php
@@ -2,32 +2,98 @@
 
 namespace TondbadSwoole\Providers\Contracts;
 
+use TondbadSwoole\Core\Config;
 use TondbadSwoole\Core\Container;
 
 class ServiceProvider
 {
-
+    /**
+     * Register services or bindings in the container.
+     * This method should be overridden in child classes to add custom services.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
     public function register(Container $container): void
     {
     }
 
+    /**
+     * Boot the services that are registered in the container.
+     * This method should be overridden in child classes to perform post-registration actions.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
     public function boot(Container $container): void
     {
     }
 
-    public function beforeRegister(Container $container)
+    /**
+     * Actions to perform before the registration of services.
+     * This method can be overridden in child classes to define pre-registration logic.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
+    public function beforeRegister(Container $container): void
     {
     }
 
-    public function afterRegister(Container $container)
+    /**
+     * Actions to perform after the registration of services.
+     * This method can be overridden in child classes to define post-registration logic.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
+    public function afterRegister(Container $container): void
     {
     }
 
-    public function beforeBoot(Container $container)
+    /**
+     * Actions to perform before booting the services.
+     * This method can be overridden in child classes to define pre-boot logic.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
+    public function beforeBoot(Container $container): void
     {
     }
 
-    public function afterBoot(Container $container)
+    /**
+     * Actions to perform after booting the services.
+     * This method can be overridden in child classes to define post-boot logic.
+     *
+     * @param Container $container The application container instance.
+     * @return void
+     */
+    public function afterBoot(Container $container): void
     {
+    }
+
+    /**
+     * Get the priority of the service provider.
+     * Providers with higher priority values are registered and booted first.
+     * This method can be overridden in child classes to specify custom priority values.
+     *
+     * @return int The priority value of the service provider. Default is 0.
+     */
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
+    /**
+     * Add a configuration file path to the global configuration search paths.
+     * This method is used to add additional configuration paths that can be loaded by the application.
+     *
+     * @param string $path The configuration file path to add.
+     * @return void
+     */
+    final protected function addConfigurationPath(string $path): void
+    {
+        Config::addToSearchPaths($path);
     }
 }
